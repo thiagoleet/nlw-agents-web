2ce267c8837619a3108304d832830b3ad4afa864
Thiago Leite <thiagofmleite@gmail.com>
2025-07-09 11:48:01 -0300

    feat: implement data fetching for room list in CreateRoomPage

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,9 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 
+type GetRoomsApiResponse = Array<{
+  id: string;
+  name: string;
+}>;
+
 export function CreateRoomPage() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-rooms"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3333/rooms");
+      const result: GetRoomsApiResponse = await response.json();
+
+      return result;
+    },
+  });
+
   return (
     <div>
       <div>Create Room</div>
+
+      {isLoading && <div>Loading...</div>}
+
+      <ul>
+        {data?.map((room) => {
+          return <li key={room.id}>{room.name}</li>;
+        })}
+      </ul>
 
       <Link
         className="text-blue-500 hover:underline"
