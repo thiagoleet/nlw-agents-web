82f6f773798e4d8f5f1b7f749f9da12dfd40f6ad
Thiago Leite <thiagofmleite@gmail.com>
2025-07-09 15:43:45 -0300

    refactor: simplify CreateRoomPage by removing unused room fetching logic

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,41 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
-import { Link } from "react-router-dom";
-
-type GetRoomsApiResponse = Array<{
-  id: string;
-  name: string;
-}>;
+import { RoomList } from "@/components/room-list";
 
 export function CreateRoomPage() {
-  const { data, isLoading } = useQuery({
-    queryKey: ["get-rooms"],
-    queryFn: async () => {
-      const response = await fetch("http://localhost:3333/rooms");
-      const result: GetRoomsApiResponse = await response.json();
-
-      return result;
-    },
-  });
-
   return (
-    <div>
-      <div>Create Room</div>
+    <main className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+          <div />
 
-      {isLoading && <div>Loading...</div>}
-
-      <div className="flex flex-col gap-1">
-        {data?.map((room) => {
-          return (
-            <Link
-              className="text-blue-500 hover:underline"
-              key={room.id}
-              to={`/room/${room.id}`}
-            >
-              {room.name}
-            </Link>
-          );
-        })}
+          <RoomList />
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
