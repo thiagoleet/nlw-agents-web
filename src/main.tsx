2e14d2dc0b58485e82ce1ff63dadee14c7a9f724
Thiago Leite <thiagofmleite@gmail.com>
2025-07-09 11:09:12 -0300

    fix: correct body class in index.html and adjust Tailwind CSS import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import "./index.css"; // Import Tailwind CSS
 import { App } from "./app";
 
 // biome-ignore lint/style/noNonNullAssertion: Mandatory for createRoot
